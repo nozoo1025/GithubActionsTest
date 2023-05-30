e24899b46877b590838956faf872874e7bd11219
nozoo1025 <nozoo1025@gmail.com>
2023-05-30 22:03:57 +0900

    使わない変数を追加

--- app/src/main/java/com/example/githubactionstest/MainActivity.kt
+++ app/src/main/java/com/example/githubactionstest/MainActivity.kt
@@ -23,6 +23,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     val message = "Hello"
+                    val message2 = ""
                     Greeting("プルリク")
                 }
             }
