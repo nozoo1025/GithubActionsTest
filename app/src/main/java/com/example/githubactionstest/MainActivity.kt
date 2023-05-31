52fc3576bcccdc2de7c2a5389658b68b84947b0b
nozomi-inoue <inoue@uniaim.co.jp>
2023-05-31 09:30:32 +0900

    不要な変数を追加

--- app/src/main/java/com/example/githubactionstest/MainActivity.kt
+++ app/src/main/java/com/example/githubactionstest/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    val name = "Android"
                     Greeting("プルリク")
                 }
             }
